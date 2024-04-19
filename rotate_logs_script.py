6f5abdd165ad8267e890df53308d976a245437d8
kkozhabay <kkozhabay@maek.kz>
2024-04-19 16:10:03 +0500

    add log file

---
+++ rotate_logs_script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import shutil
+import datetime
+
+def rotate_logs(log_path, max_size=50 * 1024 * 1024, backup_count=5):
+    if os.path.exists(log_path) and os.path.getsize(log_path) > max_size:
+        backup_file = f"{log_path}.{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.bak"
+        shutil.move(log_path, backup_file)
+        for i in range(backup_count, 0, -1):
+            prev_backup = f"{log_path}.{i - 1}.bak"
+            new_backup = f"{log_path}.{i}.bak"
+            if os.path.exists(prev_backup):
+                shutil.move(prev_backup, new_backup)
+
+# Пример использования
+log_path = '/home/maek_zakup/logs/gunicorn_access.log'
+rotate_logs(log_path)
+log_path = '/home/maek_zakup/logs/gunicorn_error.log'
+rotate_logs(log_path)
