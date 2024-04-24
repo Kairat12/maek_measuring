4b0621e66bf062237ef100fdc9a87d60798f8d9f
kkozhabay <kkozhabay@maek.kz>
2024-04-24 11:33:33 +0500

    add new search

--- sklad/views.py
+++ sklad/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.db.models import Sum
+from django.http import QueryDict
 from django.shortcuts import render
 
 from sklad.models import MainSklad
@@ -14,23 +15,27 @@ def index(request):
 
 def main_sklad(request):
     search_params = {
-        'item_number__icontains': request.GET.get('item_number'),
-        'name__icontains': request.GET.get('name'),
+        'item_number__icontains': request.GET.get('item_number__icontains'),
+        'name__icontains': request.GET.get('name__icontains'),
         'price__gte': request.GET.get('price__gte'),
         'price__lte': request.GET.get('price__lte'),
         'sum__gte': request.GET.get('sum__gte'),
         'sum__lte': request.GET.get('sum__lte'),
         'date_receipt__gte': request.GET.get('date_receipt__gte'),
         'date_receipt__lte': request.GET.get('date_receipt__lte'),
-        'number_sklad__icontains': request.GET.get('number_sklad'),
-        'responsible__icontains': request.GET.get('responsible'),
-        'contractor__icontains': request.GET.get('contractor'),
+        'number_sklad__icontains': request.GET.get('number_sklad__icontains'),
+        'responsible__icontains': request.GET.get('responsible__icontains'),
+        'contractor__icontains': request.GET.get('contractor__icontains'),
     }
+    search_querydict = QueryDict(mutable=True)
+    for key, value in search_params.items():
+        if value:
+            search_querydict[key] = value
 
     main_sklads = MainSklad.objects.all().order_by('id')
     for field, value in search_params.items():
         if value:
-            if field == 'price__gte' or field == 'price__lte' or field == 'sum__gte' or field == 'sum__lte':
+            if field.startswith(('price', 'sum')):
                 main_sklads = main_sklads.filter(**{field: float(value.replace(',', '.'))})
             else:
                 main_sklads = main_sklads.filter(**{field: value})
@@ -52,8 +57,10 @@ def main_sklad(request):
         main_sklads = paginator.page(paginator.num_pages)
 
 
+    print("search_querydict", search_querydict)
 
     return render(request, 'main_sklad.html', {'main_sklads': main_sklads,
                                                'main_sklads_count': main_sklads_count,
                                                'main_sklads_sum': main_sklads_sum,
+                                               'search_querydict': search_querydict.urlencode(),
                                                })
