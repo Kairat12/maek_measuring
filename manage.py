8635e663eedf56628b559458559f8f4eaf3427ea
kkozhabay12 <kkozhabay@maek.kz>
2024-06-25 16:14:55 +0500

    first commit

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maek_sklad.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maek_measuring.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
