ead9c761464dfeb9b1a7a74c59410e6c79ef9ca2
kkozhabay12 <kkozhabay@maek.kz>
2024-06-27 11:32:21 +0500

    add deviceses

--- devices/admin.py
+++ devices/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from devices.models import Device
+
+
 # Register your models here.
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    search_fields = ['name']
