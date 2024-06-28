69961b3767347d074133f5c3e6b335da39f6039d
kkozhabay12 <kkozhabay@maek.kz>
2024-06-28 14:49:35 +0500

    add deviceses

--- devices/models.py
+++ devices/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Device(models.Model):
     name = models.CharField(max_length=400, verbose_name="Наименование")
     subdivision = models.CharField(max_length=400, verbose_name="Подразделение", null=True, blank=True)
-    factory_number = models.CharField(max_length=200,verbose_name="Заводской Номер", null=True, blank=True)
+    factory_number = models.CharField(max_length=200, verbose_name="Заводской Номер", null=True, blank=True)
     sensor = models.CharField(max_length=200, verbose_name="Датчик", null=True, blank=True)
     quantity = models.IntegerField(verbose_name="количество", null=True, blank=True)
     date_issued = models.DateField(verbose_name="Дата Выпуска", null=True, blank=True)
@@ -13,3 +14,6 @@ class Device(models.Model):
     repair_result = models.CharField(max_length=200, verbose_name="Результат Ремонта", null=True, blank=True)
     date_next_verification = models.DateField(verbose_name="Дата Следующей Поверки", null=True, blank=True)
     state = models.CharField(max_length=100, verbose_name="Состояние", null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}-{self.subdivision}-{self.factory_number}'
